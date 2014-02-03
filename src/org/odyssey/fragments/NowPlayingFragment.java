a925e6b42f0ad34e3f15d2462b4887148fe1806a
gnome17 <frederik.luetkes@googlemail.com>
2014-02-03 19:51:36 +0100

    implement functions of nowplaying fragment; update seekbar still open issue

--- src/org/odyssey/fragments/NowPlayingFragment.java
+++ src/org/odyssey/fragments/NowPlayingFragment.java
@@ -1,43 +1,157 @@
 package org.odyssey.fragments;
 
-import org.odyssey.MusicLibraryHelper;
+import org.odyssey.NowPlayingInformation;
 import org.odyssey.OdysseyApplication;
 import org.odyssey.R;
+import org.odyssey.playbackservice.IOdysseyPlaybackService;
+import org.odyssey.playbackservice.PlaybackService;
 import org.odyssey.playbackservice.TrackItem;
 
+import android.app.Activity;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.RemoteException;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class NowPlayingFragment extends Fragment implements OnSeekBarChangeListener{
+public class NowPlayingFragment extends Fragment implements OnSeekBarChangeListener, OdysseyApplication.NowPlayingListener {
 
 	private TextView mTitleTextView;
 	private TextView mMinDuration;
 	private TextView mMaxDuration;
 	private ImageView mCoverImageView;
 	private SeekBar mSeekBar;
+	private IOdysseyPlaybackService mPlayer;
+	private Handler seekHandler = new Handler();
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
 		View rootView = inflater.inflate(R.layout.fragment_now_playing, container, false);
 		
-		OdysseyApplication mainApplication = (OdysseyApplication) getActivity().getApplication();
+		mTitleTextView = (TextView) rootView.findViewById(R.id.nowPlayingTitleView);
 		
+		mCoverImageView = (ImageView) rootView.findViewById(R.id.nowPlayingAlbumImageView);
+		
+		mMinDuration = (TextView) rootView.findViewById(R.id.nowPlayingMinValue);
+		
+		mMinDuration.setText("0:00");
+		
+		mMaxDuration = (TextView) rootView.findViewById(R.id.nowPlayingMaxValue);		
+		
+		mSeekBar = (SeekBar) rootView.findViewById(R.id.nowPlayingSeekBar);
+		
+		// set listener for seekbar
+		mSeekBar.setOnSeekBarChangeListener(this);
+		
+		// get the playbackservice
+		mPlayer = ((OdysseyApplication) getActivity().getApplication()).getPlaybackService();
+		
+		// Set up button listeners
+		rootView.findViewById(R.id.nowPlayingNextButton).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				try {
+					mPlayer.next();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		rootView.findViewById(R.id.nowPlayingPreviousButton).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				try {
+					mPlayer.previous();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
+		rootView.findViewById(R.id.nowPlayingPlaypauseButton).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				try {
+					mPlayer.togglePause();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		rootView.findViewById(R.id.nowPlayingStopButton).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				try {
+					mPlayer.stop();
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		//TODO change repeat behavior to toggle track, playlist, nothing
+		rootView.findViewById(R.id.nowPlayingRepeatButton).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				try {
+					mPlayer.setRepeat(!mPlayer.getRepeat());
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		//TODO change shuffle behavior
+		rootView.findViewById(R.id.nowPlayingShuffleButton).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				try {
+					mPlayer.setRandom(!mPlayer.getRandom());
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});	
+		
+		//register for playback callbacks
+		OdysseyApplication mainApplication = (OdysseyApplication) getActivity().getApplication();
+
+		mainApplication.registerNowPlayingListener(this);
+		
+		return rootView;
+	}
+	
+	private void updateStatus() {
+		
+		// get current track
 		TrackItem currentTrack = null;
 		try {
-			currentTrack = mainApplication.getPlaybackService().getCurrentSong();
+			currentTrack = mPlayer.getCurrentSong();
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,11 +159,9 @@ public class NowPlayingFragment extends Fragment implements OnSeekBarChangeListe
 		
 		if(currentTrack == null) {
 			currentTrack = new TrackItem();
-		}
+		}		
 		
 		// set tracktitle and albumcover
-		mTitleTextView = (TextView) rootView.findViewById(R.id.nowPlayingTitleView);
-		
 		mTitleTextView.setText(currentTrack.getTrackTitle());
 		
 		String where = android.provider.MediaStore.Audio.Albums.ALBUM + "=?";	
@@ -62,23 +174,15 @@ public class NowPlayingFragment extends Fragment implements OnSeekBarChangeListe
 		String coverPath = null;
 		if(cursor.moveToFirst()) {
 			coverPath = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART));
-		} 
-		
-		mCoverImageView = (ImageView) rootView.findViewById(R.id.nowPlayingAlbumImageView);
+		} 		
 		
 		if(coverPath != null) {
 			Drawable tempImage = Drawable.createFromPath(coverPath);
 			mCoverImageView.setImageDrawable(tempImage);
 		} else {
 			mCoverImageView.setImageResource(R.drawable.coverplaceholder);
-		}
+		}	
 		
-		// set min and max value
-		mMinDuration = (TextView) rootView.findViewById(R.id.nowPlayingMinValue);
-		
-		mMinDuration.setText("0:00");
-		
-		mMaxDuration = (TextView) rootView.findViewById(R.id.nowPlayingMaxValue);
 		// calculate duration in minutes and seconds
 		String seconds = String.valueOf((currentTrack.getTrackDuration() % 60000) / 1000);
 
@@ -90,37 +194,45 @@ public class NowPlayingFragment extends Fragment implements OnSeekBarChangeListe
 			mMaxDuration.setText(minutes + ":" + seconds);
 		}			
 		
-		// set listener for seekbar
-		mSeekBar = (SeekBar) rootView.findViewById(R.id.nowPlayingSeekBar);
+		// set up seekbar
+		mSeekBar.setMax((int) currentTrack.getTrackDuration());	
 		
-		mSeekBar.setOnSeekBarChangeListener(this);
-		
-		mSeekBar.setMax((int) currentTrack.getTrackDuration());
-		
+		updateSeekBar();
+	}
+	
+	private void updateSeekBar() {
 		try {
-			mSeekBar.setProgress(mainApplication.getPlaybackService().getTrackPosition());
+			mSeekBar.setProgress(mPlayer.getTrackPosition());
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return rootView;
+		//FIXME add termination condition
+		//seekHandler.postDelayed(seekBarRunnable, 1000);
 	}
+	
+	//TODO improve this
+	Runnable seekBarRunnable = new Runnable() {
+		   
+		@Override
+		public void run() {
+			updateSeekBar();
+		}
+	};
+	
 
 	@Override
 	public void onProgressChanged(SeekBar seekBar, int progress,
 			boolean fromUser) {
 
 		if (fromUser) {
-			OdysseyApplication mainApplication = (OdysseyApplication) getActivity().getApplication();
 			try {
-				mainApplication.getPlaybackService().seekTo(progress);
+				mPlayer.seekTo(progress);
 			} catch (RemoteException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-	
 	}
 
 	@Override
@@ -134,5 +246,23 @@ public class NowPlayingFragment extends Fragment implements OnSeekBarChangeListe
 		// TODO Auto-generated method stub
 		
 	}
-	
+
+	@Override
+	public void onNewInformation(NowPlayingInformation info) {
+		
+		new Thread() {
+			public void run() {
+				Activity activity = (Activity) getActivity();
+				if (activity != null) {
+					activity.runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							// update views
+							updateStatus();
+						}
+					});
+				}
+			}
+		}.start();
+	}
 }
