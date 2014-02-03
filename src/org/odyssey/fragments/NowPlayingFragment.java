959135ac785b1a8299d2338f0a2d93f6545c5ea8
gnome17 <frederik.luetkes@googlemail.com>
2014-02-03 15:53:27 +0100

    add layout for nowplaying fragment

--- src/org/odyssey/fragments/NowPlayingFragment.java
+++ src/org/odyssey/fragments/NowPlayingFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class NowPlayingFragment extends Fragment {
@@ -14,11 +15,23 @@ public class NowPlayingFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
-		View rootView = inflater.inflate(R.layout.fragment_section_dummy, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_now_playing, container, false);
 		
-		TextView text = (TextView) rootView.findViewById(android.R.id.text1);
+		TextView title = (TextView) rootView.findViewById(R.id.nowPlayingTitleView);
 		
-		text.setText(R.string.dummy_section_text);
+		title.setText(R.string.dummy_section_text);
+		
+		TextView minValue = (TextView) rootView.findViewById(R.id.nowPlayingMinValue);
+		
+		minValue.setText("00:00");
+		
+		TextView maxValue = (TextView) rootView.findViewById(R.id.nowPlayingMaxValue);
+		
+		maxValue.setText("42:42");		
+		
+		ImageView image = (ImageView) rootView.findViewById(R.id.nowPlayingAlbumImageView);
+		
+		image.setImageResource(R.drawable.coverplaceholder);
 		
 		return rootView;
 	}
