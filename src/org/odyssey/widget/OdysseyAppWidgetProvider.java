1e43f3a3881795401b81a61df9705439e52a0800
gnome17 <frederik.luetkes@googlemail.com>
2014-02-07 15:11:42 +0100

    add onreceive to widgetprovider; leads to app crash

--- src/org/odyssey/widget/OdysseyAppWidgetProvider.java
+++ src/org/odyssey/widget/OdysseyAppWidgetProvider.java
@@ -1,24 +1,30 @@
 package org.odyssey.widget;
 
+import java.util.ArrayList;
+
 import org.odyssey.MainActivity;
+import org.odyssey.NowPlayingInformation;
 import org.odyssey.R;
+import org.odyssey.playbackservice.PlaybackService;
+import org.odyssey.playbackservice.TrackItem;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Parcelable;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class OdysseyAppWidgetProvider extends AppWidgetProvider {
 	
-	public static final String ACTION_PLAY = "org.odyssey.play";
-	public static final String ACTION_PAUSE = "org.odyssey.pause";
-	public static final String ACTION_NEXT = "org.odyssey.next";
-	public static final String ACTION_PREVIOUS = "org.odyssey.previous";
-	public static final String ACTION_STOP = "org.odyssey.stop";	
-
+	@Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		
         final int N = appWidgetIds.length;
 
         // Perform this loop procedure for each App Widget that belongs to this provider
@@ -34,26 +40,26 @@ public class OdysseyAppWidgetProvider extends AppWidgetProvider {
             views.setOnClickPendingIntent(R.id.odysseyWidgetImageView, mainPendingIntent);
             
             // Play/Pause action
-            Intent playIntent = new Intent(ACTION_PLAY);
+            Intent playIntent = new Intent(PlaybackService.ACTION_PLAY);
             PendingIntent playPendingIntent = PendingIntent.getBroadcast(context, 42, playIntent, PendingIntent.FLAG_UPDATE_CURRENT); 
             views.setOnClickPendingIntent(R.id.odysseyWidgetPlaypauseButton, playPendingIntent);
             
-//			Intent pauseIntent = new Intent(ACTION_PAUSE);
+//			Intent pauseIntent = new Intent(PlaybackService.ACTION_PAUSE);
 //			PendingIntent pausePendingIntent = PendingIntent.getBroadcast(context, 42, pauseIntent, PendingIntent.FLAG_UPDATE_CURRENT);            
 //			views.setOnClickPendingIntent(R.id.odysseyWidgetPlaypauseButton, pausePendingIntent);
 			
             // Previous song action
-    		Intent prevIntent = new Intent(ACTION_PREVIOUS);
+    		Intent prevIntent = new Intent(PlaybackService.ACTION_PREVIOUS);
     		PendingIntent prevPendingIntent = PendingIntent.getBroadcast(context, 42, prevIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             views.setOnClickPendingIntent(R.id.odysseyWidgetPreviousButton, prevPendingIntent);
             
     		// Next song action
-    		Intent nextIntent = new Intent(ACTION_NEXT);
+    		Intent nextIntent = new Intent(PlaybackService.ACTION_NEXT);
     		PendingIntent nextPendingIntent = PendingIntent.getBroadcast(context, 42, nextIntent, PendingIntent.FLAG_UPDATE_CURRENT);            
     		views.setOnClickPendingIntent(R.id.odysseyWidgetNextButton, nextPendingIntent);
     		
     		// Quit action
-    		Intent stopIntent = new Intent(ACTION_STOP);
+    		Intent stopIntent = new Intent(PlaybackService.ACTION_STOP);
     		PendingIntent stopPendingIntent = PendingIntent.getBroadcast(context, 42, stopIntent, PendingIntent.FLAG_UPDATE_CURRENT);    		
             views.setOnClickPendingIntent(R.id.odysseyWidgetStopButton, stopPendingIntent);
             
@@ -62,6 +68,30 @@ public class OdysseyAppWidgetProvider extends AppWidgetProvider {
         }
     }
 
-	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		super.onReceive(context, intent);		
+		
+		if(intent.getAction().equals(PlaybackService.MESSAGE_NEWTRACKINFORMATION)) {
+			
+			intent.setExtrasClassLoader(context.getClassLoader());
+			
+            // get remoteviews
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.odyssey_appwidget);  			
+			
+			ArrayList<Parcelable> arrayList = intent.getParcelableArrayListExtra(PlaybackService.INTENT_TRACKITEMNAME);
+			
+			if(arrayList.size() == 2) {
+				TrackItem item = (TrackItem) arrayList.get(0);
+				NowPlayingInformation playInfo = (NowPlayingInformation) arrayList.get(1);
+				
+				String text = item.getTrackArtist() + " - " +item.getTrackTitle();
+				
+				views.setTextViewText(R.id.odysseyWidgetTextView, text);
+			}
+		}
+		
+	}
 	
 }
