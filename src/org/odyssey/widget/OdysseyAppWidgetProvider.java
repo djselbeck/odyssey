06af3a8999b934afcf8909d1c8ed1ef64820029a
gnome17 <frederik.luetkes@googlemail.com>
2014-02-07 13:07:10 +0100

    add widget stuff

---
+++ src/org/odyssey/widget/OdysseyAppWidgetProvider.java
@@ -0,0 +1,16 @@
+package org.odyssey.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class OdysseyAppWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        
+    	super.onUpdate(context, appWidgetManager, appWidgetIds);
+    	
+    }
+	
+	
+}
