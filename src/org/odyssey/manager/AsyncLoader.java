7b86a4402807baed9f9eb4f9377a02208aab7aae
djselbeck <hendrikborghorst@googlemail.com>
2014-02-09 23:12:02 +0100

    Moved caching out of gui thread

--- src/org/odyssey/manager/AsyncLoader.java
+++ src/org/odyssey/manager/AsyncLoader.java
@@ -12,8 +12,7 @@ import android.widget.TextView;
 /*
  * Loaderclass for covers
  */
-public class AsyncLoader extends
-		AsyncTask<AsyncLoader.CoverViewHolder, Void, Bitmap> {
+public class AsyncLoader extends AsyncTask<AsyncLoader.CoverViewHolder, Void, Bitmap> {
 
 	private CoverViewHolder cover;
 
@@ -35,55 +34,57 @@ public class AsyncLoader extends
 		cover = params[0];
 
 		if (cover.imagePath != null) {
-//			Bitmap tempImage = BitmapFactory.decodeFile(params[0].imagePath);
-//
-//			return tempImage;
-			
-			return decodeSampledBitmapFromResource(params[0].imagePath, cover.coverViewReference.get().getWidth(), 
-													cover.coverViewReference.get().getHeight());
+			// Bitmap tempImage = BitmapFactory.decodeFile(params[0].imagePath);
+			//
+			// return tempImage;
+
+			return decodeSampledBitmapFromResource(params[0].imagePath, cover.coverViewReference.get().getWidth(), cover.coverViewReference.get().getHeight());
 		}
 
 		return null;
 
 	}
-	
-    public static Bitmap decodeSampledBitmapFromResource(String pathName,
-            int reqWidth, int reqHeight) {
 
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(pathName, options);
+	public Bitmap decodeSampledBitmapFromResource(String pathName, int reqWidth, int reqHeight) {
 
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(pathName, options);
 
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(pathName, options);
-    }    
-    
-    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-	    // Raw height and width of image
-	    final int height = options.outHeight;
-	    final int width = options.outWidth;
-	    int inSampleSize = 1;
-	
-	    if (height > reqHeight || width > reqWidth) {
-	
-	        final int halfHeight = height / 2;
-	        final int halfWidth = width / 2;
-	
-	        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-	        // height and width larger than the requested height and width.
-	        while ((halfHeight / inSampleSize) > reqHeight
-	                && (halfWidth / inSampleSize) > reqWidth) {
-	            inSampleSize *= 2;
-	        }
-	    }
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
 
-    	return inSampleSize;
-    }	
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		Bitmap retBitmap = BitmapFactory.decodeFile(pathName, options);
+		if (cover.cache != null) {
+			cover.cache.get().put(cover.imagePath, retBitmap);
+		}
+		return retBitmap;
+	}
+
+	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			final int halfHeight = height / 2;
+			final int halfWidth = width / 2;
+
+			// Calculate the largest inSampleSize value that is a power of 2 and
+			// keeps both
+			// height and width larger than the requested height and width.
+			while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+				inSampleSize *= 2;
+			}
+		}
+
+		return inSampleSize;
+	}
 
 	@Override
 	protected void onPostExecute(Bitmap result) {
@@ -92,9 +93,6 @@ public class AsyncLoader extends
 
 		// set cover if exists
 		if (cover.coverViewReference != null && result != null) {
-			if(cover.cache != null) {
-				cover.cache.get().put(cover.imagePath, result);
-			}
 			cover.coverViewReference.get().setImageBitmap(result);
 		}
 
