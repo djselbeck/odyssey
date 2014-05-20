6eb375dc3975c24cb15aa6a94463caff429650da
gnome17 <frederik.luetkes@googlemail.com>
2014-05-20 22:56:23 +0200

    add new loader to load album cover for artistssection

--- src/org/odyssey/manager/ArtistCoverLoader.java
+++ src/org/odyssey/manager/ArtistCoverLoader.java
@@ -4,11 +4,16 @@ import org.odyssey.MusicLibraryHelper;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.CursorJoiner;
+import android.database.MatrixCursor;
 import android.provider.MediaStore;
-import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.CursorLoader;
 import android.util.Log;
 
-public class ArtistCoverLoader extends AsyncTaskLoader<Cursor> {
+/*
+ * Custom Loader for ARTIST with ALBUM_ART
+ */
+public class ArtistCoverLoader extends CursorLoader {
 
     private static final String TAG = "ArtistCoverLoader";
 
@@ -23,15 +28,53 @@ public class ArtistCoverLoader extends AsyncTaskLoader<Cursor> {
     public Cursor loadInBackground() {
         Log.v(TAG, "load ArtistCovers");
 
-        // TODO still unfinished
+        // get all album covers with corresponding artist
+        Cursor albumArtCursor = mContext.getContentResolver().query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, new String[] { MediaStore.Audio.Albums.ALBUM_ART, android.provider.MediaStore.Audio.Albums.ARTIST }, "", null,
+                MediaStore.Audio.Albums.ARTIST + " COLLATE NOCASE");
 
-        // get all albums
-        String[] projectionArgs = { MediaStore.Audio.Albums.ALBUM_ART, android.provider.MediaStore.Audio.Albums.ARTIST };
-
-        Cursor albumArtCursor = mContext.getContentResolver().query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, projectionArgs, "", null, MediaStore.Audio.Albums.ARTIST + " COLLATE NOCASE");
-
+        // get all artists
         Cursor artistsCursor = mContext.getContentResolver().query(MediaStore.Audio.Artists.EXTERNAL_CONTENT_URI, MusicLibraryHelper.projectionArtists, "", null, MediaStore.Audio.Artists.ARTIST + " COLLATE NOCASE");
 
-        return null;
+        // create a custom cursor to mix both
+        MatrixCursor artistsCoverCursor = new MatrixCursor(new String[] { MediaStore.Audio.Artists.ARTIST, MediaStore.Audio.Artists.ARTIST_KEY, MediaStore.Audio.Artists.NUMBER_OF_TRACKS, MediaStore.Audio.Artists._ID,
+                MediaStore.Audio.Artists.NUMBER_OF_ALBUMS, MediaStore.Audio.Albums.ALBUM_ART });
+
+        // join both cursor if match is found
+        CursorJoiner artistCoverJoiner = new CursorJoiner(artistsCursor, new String[] { MediaStore.Audio.Artists.ARTIST }, albumArtCursor, new String[] { MediaStore.Audio.Albums.ARTIST });
+        String artist, artistKey, cover;
+        int numberOfTracks, numberOfAlbums;
+        long artistID;
+
+        for (CursorJoiner.Result joinerResult : artistCoverJoiner) {
+            switch (joinerResult) {
+            case LEFT:
+                // no cover found
+                artist = artistsCursor.getString(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.ARTIST));
+                artistKey = artistsCursor.getString(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.ARTIST_KEY));
+                numberOfTracks = artistsCursor.getInt(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.NUMBER_OF_TRACKS));
+                artistID = artistsCursor.getLong(artistsCursor.getColumnIndex(MediaStore.Audio.Artists._ID));
+                numberOfAlbums = artistsCursor.getInt(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.NUMBER_OF_ALBUMS));
+                cover = null;
+                artistsCoverCursor.addRow(new Object[] { artist, artistKey, numberOfTracks, artistID, numberOfAlbums, cover });
+                break;
+            case RIGHT:
+                // only cover found
+                break;
+            case BOTH:
+                // artist and album cover match
+                artist = artistsCursor.getString(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.ARTIST));
+                artistKey = artistsCursor.getString(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.ARTIST_KEY));
+                numberOfTracks = artistsCursor.getInt(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.NUMBER_OF_TRACKS));
+                artistID = artistsCursor.getLong(artistsCursor.getColumnIndex(MediaStore.Audio.Artists._ID));
+                numberOfAlbums = artistsCursor.getInt(artistsCursor.getColumnIndex(MediaStore.Audio.Artists.NUMBER_OF_ALBUMS));
+                cover = albumArtCursor.getString(albumArtCursor.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART));
+                artistsCoverCursor.addRow(new Object[] { artist, artistKey, numberOfTracks, artistID, numberOfAlbums, cover });
+                break;
+            }
+        }
+
+        // return new custom cursor
+        return artistsCoverCursor;
     }
+
 }
